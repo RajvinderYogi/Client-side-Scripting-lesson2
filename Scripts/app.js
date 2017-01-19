00ab0472851e001bf0e8b97e250429b2f3cbf9a5
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-01-19 03:04:50 -0500

    fixed navigation bar

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,2 +1,12 @@
 /* custom Java Script here*/
 console.log("App is Started!");
+// create a refrence to the button of page
+var clickMeButton = document.getElementById("clickMeButton");
+
+//add an  event listner for click event and call the function
+clickMeButton.addEventListener("click, Click");
+
+// click function used an event listner
+function Click(){
+console.log("clicked!");
+}
