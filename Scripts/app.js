50de0faab9474b99a95fb9eba04752ea4aac2c1c
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-01-22 01:12:32 -0500

    final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,13 @@
 /* custom Java Script here*/
-console.log("App is Started!");
-// create a refrence to the button of page
+console.log("App Started");
+
+// create a reference to the button on the page (index.html)
 var clickMeButton = document.getElementById("clickMeButton");
 
-//add an  event listner for click event and call the function
-clickMeButton.addEventListener("click, Click");
+// add an event listener - for the click event and call the Click function
+clickMeButton.addEventListener("click", Click);
 
-// click function used an event listner
-function Click(){
-console.log("clicked!");
+// click function - used as an event handler
+function Click() {
+ console.log("clicked!");
 }
